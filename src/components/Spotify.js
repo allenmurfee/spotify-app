d0eebb700ddbe5c763dbf20a85ed0312685e7a10
allenmurfee <allenmurfee123@gmail.com>
2023-05-03 15:34:22 -0400

    POST request

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -6,14 +6,16 @@ function getSpotify() {
   const base64Auth = btoa(`${clientId}:${clientSecret}`);
   const tokenUrl = "https://accounts.spotify.com/api/token";
 
-  const authOptions = {
+  fetch(tokenUrl, {
     method: "POST",
     headers: {
       Authorization: `Basic ${base64Auth}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: "grant_type=client_credentials",
-  };
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
 }
 
 export default getSpotify;
